c680bcef1e150a23fa2e15ff92c077bdc4185c95
Jiimjeam <2201105872@student.buksu.edu.ph>
2024-10-29 15:21:59 +0800

    added Folders

---
+++ BackEnd/BackEnd/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json()); //middleware
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is running in port: ", PORT);
+});
+
+//Cax9XvoodE358aYV
+
