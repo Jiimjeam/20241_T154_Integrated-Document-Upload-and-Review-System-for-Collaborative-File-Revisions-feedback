d73dba90cc1991b9a0f85763c299b82a3a6d4999
Jiimjeam <2201105872@student.buksu.edu.ph>
2024-11-12 12:38:47 +0800

    fixed landingpage

--- FrontEnd/FrontEnd/src/components/LandingPage/body.jsx
+++ FrontEnd/FrontEnd/src/components/LandingPage/body.jsx
@@ -1,44 +1,56 @@
-import React from 'react'
-import './body.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './body.css';
 
 import logo from '../../assets/file-svg.svg';
 import CITL from '../../assets/CITLlogo.jpg';
 import Navbar from './navbar';
-
-
-import TypeWriter from './typewriterAnimation'
+import TypeWriter from './typewriterAnimation';
 
 const Body = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    console.log("LOGIN NAVIGATE")
+    navigate('/login'); 
+  };
+
+  const handleSignUpClick = () => {
+    console.log("SIGNUP NAVIGATE")
+    navigate('/signup');
+  };
+
   return (
-    <>
-    <Navbar/>
-    <div className='header'>
-     
-      {/* <h1>Syllabus <span>Review</span></h1> */}
-      {/* <TypeWriter /> */}
-      <div className='relevantAuth'>
-        <h3>Relevant Authrities</h3>
-        <img src={CITL} alt="Nasty-Image" className="CITL-logo" /> 
-        <p>Buksu CITL</p>
-      </div>
-      <img src={logo} alt="File.svg" className="logo" /> 
+    <main>
+      <Navbar />
 
+      <header className="header">
+        <TypeWriter />
+        <div className="relevantAuth">
+          <h3>Relevant Authorities</h3>
+          <img src={CITL} alt="CITL Logo" className="CITL-logo" /> 
+          <p>Buksu CITL</p>
+        </div>
+        <img src={logo} alt="Document Icon" className="logo" />
+      </header>
 
-
-      <section className='section1' id='1'>
-        <div className='section-1-container'>
-            <h2>About The <span>Syllabus</span></h2>
-            <p>
+      <section className="section1" id="1">
+        <div className="section-1-container">
+          <h2>About The <span>Syllabus</span></h2>
+          <p>
             Our platform streamlines syllabus checking and finalization within Bukidnon State University, 
             offering a dedicated environment for instructors and syllabus checkers to review, update, and finalize syllabus content efficiently.
-            </p>
+          </p>
+          {/* Login and Signup buttons */}
+          <div className="auth-buttons">
+
+            <button onClick={handleLoginClick}>Login</button>
+            <button onClick={handleSignUpClick}>Sign Up</button>
+          </div>
         </div>
       </section>
-    </div>
+    </main>
+  );
+};
 
-    
-    </>
-  )
-}
-
-export default Body
+export default Body;
