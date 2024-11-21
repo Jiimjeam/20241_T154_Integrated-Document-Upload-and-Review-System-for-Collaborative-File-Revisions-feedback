c2bf854208ae71f68784b36887617b6a6ee061d6
renesale12 <2201102843@student.buksu.edu.ph>
2024-11-21 16:04:48 +0800

    Update

--- FrontEnd/FrontEnd/src/components/INTR/INTRdashboard.jsx
+++ FrontEnd/FrontEnd/src/components/INTR/INTRdashboard.jsx
@@ -7,39 +7,39 @@ import FileUpload from './FileUpload'; // Updated path to FileUpload
 import Profileupload from './profile';
 
 const INTRdashboard = () => {
-    const { user, logout } = useAuthStore();
+  const { user, logout } = useAuthStore();
 
-    const handleLogout = () => {
-        logout();
-    };
+  const handleLogout = () => {
+    logout();
+  };
 
-    return (
-        <div className="dashboard-container">
-            <aside className="sidebar">
-                <div className="profile-section">
-                    <Profileupload />
-                    <h3 className='profile-title'>Profile Information</h3>
-                    <p className='profile-detail'>Name: {user.name}</p>
-                    <p className='profile-detail'>Email: {user.email}</p>
-                </div>
-                <nav className="menu">
-                    <div className="menuItem">
-                        <img src={home} alt="home" className="menu-icon" />Home
-                    </div>
-                    <div className="menuItem">
-                        <img src={Notifications} alt="Notifications" className="menu-icon" />Notifications
-                    </div>
-                    <div className="menuItem">
-                        <img src={History} alt="History" className="menu-icon" /> History
-                    </div>
-                </nav>
-                <button className="logoutButton" onClick={handleLogout}>Logout</button>
-            </aside>
-            <main className="main-content">
-                <FileUpload /> {/* File upload component */}
-            </main>
+  return (
+    <div className="dashboard-container">
+      <aside className="sidebar">
+        <div className="profile-section">
+          <Profileupload />
+          <h3 className='profile-title'>Profile Information</h3>
+          <p className='profile-detail'>Name: {user.name}</p>
+          <p className='profile-detail'>Email: {user.email}</p>
         </div>
-    );
+        <nav className="menu">
+          <div className="menuItem">
+            <img src={home} alt="home" className="menu-icon" />Home
+          </div>
+          <div className="menuItem">
+            <img src={Notifications} alt="Notifications" className="menu-icon" />Notifications
+          </div>
+          <div className="menuItem">
+            <img src={History} alt="History" className="menu-icon" /> History
+          </div>
+        </nav>
+        <button className="logoutButton" onClick={handleLogout}>Logout</button>
+      </aside>
+      <main className="main-content">
+        <FileUpload /> {/* File upload component */}
+      </main>
+    </div>
+  );
 };
 
 export default INTRdashboard;
